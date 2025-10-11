15abe8bcc897358de24aa1fe025d296f999c63ff
MathiasBaumgartinger <33001106+MathiasBaumgartinger@users.noreply.github.com>
2025-10-11 08:38:44 +0000

    Deploying to gh-pages from @ MathiasBaumgartinger/al-folio@43fae297fa896261fcb8f69d1354d147f99b0a28 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,394 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/al-folio/";
+    },
+  },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/blog/";
+          },
+        },{id: "nav-dissemination",
+          title: "Dissemination",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/publications/";
+          },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "Open projects and contributions I have worked on.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/cv/";
+          },
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/al-folio/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/al-folio/blog/";
+              },
+            },{id: "post-a-post-with-plotly-js",
+        
+          title: "a post with plotly.js",
+        
+        description: "this is what included plotly.js code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2025/plotly/";
+          
+        },
+      },{id: "post-a-post-with-image-galleries",
+        
+          title: "a post with image galleries",
+        
+        description: "this is what included image galleries could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/photo-gallery/";
+          
+        },
+      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
+        
+          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
+          
+        },
+      },{id: "post-a-post-with-tabs",
+        
+          title: "a post with tabs",
+        
+        description: "this is what included tabs in a post could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/tabs/";
+          
+        },
+      },{id: "post-a-post-with-typograms",
+        
+          title: "a post with typograms",
+        
+        description: "this is what included typograms code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/typograms/";
+          
+        },
+      },{id: "post-a-post-that-can-be-cited",
+        
+          title: "a post that can be cited",
+        
+        description: "this is what a post that can be cited looks like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/post-citation/";
+          
+        },
+      },{id: "post-a-post-with-pseudo-code",
+        
+          title: "a post with pseudo code",
+        
+        description: "this is what included pseudo code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/pseudocode/";
+          
+        },
+      },{id: "post-a-post-with-vega-lite",
+        
+          title: "a post with vega lite",
+        
+        description: "this is what included vega lite code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2024/vega-lite/";
+          
+        },
+      },{id: "post-a-post-with-custom-blockquotes",
+        
+          title: "a post with custom blockquotes",
+        
+        description: "an example of a blog post with custom blockquotes",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2023/custom-blockquotes/";
+          
+        },
+      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
+        
+          title: "displaying beautiful tables with Bootstrap Tables",
+        
+        description: "an example of how to use Bootstrap Tables",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2023/tables/";
+          
+        },
+      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+        
+          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          
+        },
+      },{id: "post-a-distill-style-blog-post",
+        
+          title: "a distill-style blog post",
+        
+        description: "an example of a distill-style blog post and main elements",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2021/distill/";
+          
+        },
+      },{id: "post-a-post-with-code",
+        
+          title: "a post with code",
+        
+        description: "an example of a blog post with some code",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/al-folio/blog/2015/code/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/al-folio/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/al-folio/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/1_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/3_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/9_project/";
+            },},{id: "projects-biopv",
+          title: 'BioPV',
+          description: "Photovoltaics, people and the biosphere: a transdisciplinary approach",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/biopv/";
+            },},{id: "projects-pa-c",
+          title: 'PA³C³',
+          description: "APV climate change mitigation assessment considering (techno-)economic, environmental and social aspects.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/pa3c3/";
+            },},{id: "projects-plusiq",
+          title: 'PlusIQ',
+          description: "APV: Integration as a path to a plus-energy district (evaluation of social acceptance by visualization).",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/plusiq/";
+            },},{id: "projects-park-working-spaces-pws",
+          title: 'Park-Working-Spaces (PWS)',
+          description: "Development of energy-autonomous, app-controlled open-space workplaces in public parks in Vienna",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/pws/";
+            },},{id: "projects-retour",
+          title: 'ReTour',
+          description: "Social acceptance of future photovoltaic and wind energy scenarios in Austrian tourism regions",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/retour/";
+            },},{id: "projects-wimby",
+          title: 'WIMBY',
+          description: "Development of innovative tools to facilitate citizen and stakeholders interaction, knowledge sharing, and collaborative evaluation of impacts, conflicts, synergies and social innovation potential of wind energy.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/academia/wimby/";
+            },},{id: "projects-body-part-fighter",
+          title: 'Body Part Fighter',
+          description: "Build creatures out of scrap body parts and fight with them!",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/game-jam/body-part-fighter/";
+            },},{id: "projects-mu-µ",
+          title: 'Mu (µ)',
+          description: "µ - A 2D platformer about shapes and home.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/game-jam/mu/";
+            },},{id: "projects-millionaire-quiz",
+          title: 'Millionaire Quiz',
+          description: "A millionaire quiz template meant for easy personalization. Makes a great fit for the next wedding, bachelor&#39;s, or birthday party or just testing your friends.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/hobby/millionaire-quiz/";
+            },},{id: "projects-geodot",
+          title: 'Geodot',
+          description: "A Godot plugin for loading geospatial data.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/work/geodot/";
+            },},{id: "projects-landscapelab",
+          title: 'LandscapeLab',
+          description: "An open kit for developing game-based participatory landscape-planning tools.",
+          section: "Projects",handler: () => {
+              window.location.href = "/al-folio/projects/work/landscapelab/";
+            },},{id: "talks-visualizing-austria-in-godot",
+          title: 'Visualizing Austria in Godot',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/al-folio/talks/godotcon/";
+            },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/mathiasbaum.bsky.social", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%6D%61%74%68%69%61%73.%62%61%75%6D%67%61%72%74%69%6E%67%65%72@%70%72%6F%74%6F%6E.%6D%65", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/mathiasBaumgartinger", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mathias-baumgartinger-0b2902196", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-8549-2806", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Mathias-Baumgartinger/", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/al-folio/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=57cyvH0AAAAJ&hl=de", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/hiasbaum", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
